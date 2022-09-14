73425bf89f048d79caee8f9186f3fe0a2add9d99
Aditya Karki <adityakarki728@gmail.com>
2022-09-13 22:23:55 -0500

    Added the markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const createTheDigits = () => {
+    const digits = []
+    for (let i=1; i < 10; i++){
+      digits.push(
+        <button>{i}</button>
+      )
+    }
+    return digits
+  }
+
+  return (
+    <div classname="app-Content">
+      <div className="container">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+        <div className="operators">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          {createTheDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
